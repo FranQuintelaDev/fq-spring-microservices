27314eb7112aef4d7dccf5212310795df01f2b94
FranQuintelaVela <fraquivel@gmail.com>
2022-12-20 11:08:20 +0100

    Updated files for fraquivel microservice deployment

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -21,6 +21,7 @@ nohup java -jar spring-petclinic-discovery-server/target/*.jar --server.port=876
 echo "Waiting for discovery server to start"
 sleep 20
 nohup java -jar spring-petclinic-customers-service/target/*.jar --server.port=8081 --spring.profiles.active=chaos-monkey > target/customers-service.log 2>&1 &
+nohup java -jar spring-fraquivel-service/target/*.jar --server.port=8081 --spring.profiles.active=chaos-monkey > target/fraquivel-service.log 2>&1 &
 nohup java -jar spring-petclinic-visits-service/target/*.jar --server.port=8082 --spring.profiles.active=chaos-monkey > target/visits-service.log 2>&1 &
 nohup java -jar spring-petclinic-vets-service/target/*.jar --server.port=8083 --spring.profiles.active=chaos-monkey > target/vets-service.log 2>&1 &
 nohup java -jar spring-petclinic-api-gateway/target/*.jar --server.port=8080 --spring.profiles.active=chaos-monkey > target/gateway-service.log 2>&1 &
